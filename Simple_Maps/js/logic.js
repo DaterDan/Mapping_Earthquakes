31e9d96001e7ee81dc600d68e8e64f6bd986702b
Dan Hedden <dirtdog1126@hotmail.com>
2020-02-19 15:23:18 -0500

    Update 5

--- Simple_Maps/js/logic.js
+++ Simple_Maps/js/logic.js
@@ -10,8 +10,17 @@ let map = L.map("mapid").setView([34.0522, -118.2437], 14); {
     color: "black"
  }).addTo(map);
 
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+	console.log(city)
+L.marker(city.location).addTo(map);
+});
+
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// let cities = L.cities([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
